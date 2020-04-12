b1edd6adfd6ae73d2eb2825d6f0d9ee717b88e6e
fredlc <fredlefevre@outlook.com>
2020-04-12 14:53:53 -0400

    Initial commit

---
+++ ARRuler/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  ARRuler
+//
+//  Created by Fred Lefevre on 2020-04-12.
+//  Copyright © 2020 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        sceneView.delegate = self
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingConfiguration()
+
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+
+   
+}

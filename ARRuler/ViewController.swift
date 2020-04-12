45661a42134ce4f6ce3c9db08b4620cc80c22561
fredlc <fredlefevre@outlook.com>
2020-04-12 16:14:26 -0400

    Function to add marker nodes and limit them to 2

--- ARRuler/ViewController.swift
+++ ARRuler/ViewController.swift
@@ -13,12 +13,13 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var markerNodes = [SCNNode]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         sceneView.delegate = self
-        
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,4 +37,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 
    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touchLocation = touches.first?.location(in: sceneView) else { return }
+        guard let hitTestResult = sceneView.hitTest(touchLocation, types: .featurePoint).first else { return }
+        addMarker(at: hitTestResult)
+    }
+    
+    func addMarker(at hitResult: ARHitTestResult) {
+        let cylinder = SCNCylinder(radius: 0.005, height: 0.02)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.blue
+        cylinder.materials = [material]
+        
+        let markerNode = SCNNode(geometry: cylinder)
+        let location = hitResult.worldTransform.columns.3
+        markerNode.position = SCNVector3(location.x, location.y, location.z)
+        sceneView.scene.rootNode.addChildNode(markerNode)
+        markerNodes.append(markerNode)
+        
+        if markerNodes.count > 2 {
+            markerNodes.first?.removeFromParentNode()
+            markerNodes.remove(at: 0)
+        }
+    }
 }
